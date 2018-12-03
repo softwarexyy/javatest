f2d965d7d6ee20e6fccdc412a691b9ae14695606
yancy <none>
2018-12-03 10:08:02 +0800

    修改注释而已

--- src/nxy/ArrayAlg.java
+++ src/nxy/ArrayAlg.java
@@ -1,12 +1,25 @@
 package nxy;
 
+/**
+ * 数组类
+ * 
+ * @author yancy
+ *
+ */
 public class ArrayAlg {
-	public static Pair<String> minmax(String []a){
-		if (a==null || a.length==0)
+	/**
+	 * 生成最小值和最大值的pair
+	 * 
+	 * @param a
+	 *            参数数组
+	 * @return
+	 */
+	public static Pair<String> minmax(String[] a) {
+		if (a == null || a.length == 0)
 			return null;
 		String min = a[0];
 		String max = a[0];
-		for (int i=0; i<a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			if (min.compareTo(a[i]) > 0)
 				min = a[i];
 			if (max.compareTo(a[i]) < 0)
@@ -14,7 +27,7 @@ public class ArrayAlg {
 		}
 		return new Pair<String>(min, max);
 	}
-	
+
 	public ArrayAlg() {
 		System.out.println("use constructor");
 	}
